ecb3a59ccba759a87e28af5569ef3116e57ddbbf
alinamichelle <alina.michelle1@gmail.com>
2025-11-25 13:06:06 -0600

    Create users table

--- db/migrate/20251125145318_create_users.rb
+++ db/migrate/20251125145318_create_users.rb
@@ -1,14 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users, id: :uuid do |t|
-      t.string :email, null: false
+      t.string :email, null: false, index: { unique: true }
       t.string :password_digest, null: false
-      t.integer :role, null: false, default: 2 # viewer
+      t.integer :role, null: false, default: 2
       t.string :name, null: false
       t.boolean :active, null: false, default: true
 
       t.timestamps
     end
-    add_index :users, :email, unique: true
   end
 end
